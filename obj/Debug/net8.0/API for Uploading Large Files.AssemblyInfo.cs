04759695f9f5d0940f4b045df625cdef46831bab
Piyush Gupta <piyushgupta135@gmail.com>
2024-11-24 17:51:18 +0530

    removed obj and bin from project

--- obj/Debug/net8.0/API for Uploading Large Files.AssemblyInfo.cs
+++ obj/Debug/net8.0/API for Uploading Large Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API for Uploading Large Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052adf95b214e13ae5eaaf6b3ed1bfcd393a8861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e25182e5c3caf3e62b85b135602f3ec52e41d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API for Uploading Large Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API for Uploading Large Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
